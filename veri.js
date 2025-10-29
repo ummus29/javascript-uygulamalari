43c3c2da829af07b7a50108d983af06a32c0666c
ÜMMÜGÜLSÜM SÖĞÜT <ummugulsum.sogut@ogr.gelisim.edu.tr>
2025-10-29 16:19:55 +0300

    Add files via upload

---
+++ veri.js
@@ -0,0 +1,9 @@
+let sayi="15";
+console.log(sayi, " Değeri: ",typeof(sayi));
+let sayi1=parseInt(sayi);
+console.log(sayi1 ,"Değeri: ",typeof(sayi1));
+let sayi2=11.5;
+console.log(sayi2 ,"Değeri: ",typeof(sayi2));
+let sayi3=55;
+let sayi4=sayi3.toString();
+console.log(sayi4 ," Değeri: ",typeof(sayi4));
