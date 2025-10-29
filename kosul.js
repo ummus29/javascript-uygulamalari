dd0dd671f7c2a83c9a44cb14df094f871a5f135b
ÜMMÜGÜLSÜM SÖĞÜT <ummugulsum.sogut@ogr.gelisim.edu.tr>
2025-10-29 21:14:56 +0300

    Add files via upload

---
+++ kosul.js
@@ -0,0 +1,10 @@
+let yas=Number(prompt("YAŞINIZI GİRİN: "));
+let butce=Number(prompt("BÜTÇENİZİ GİRİN: "));
+if (yas>18 && butce>2000)
+{
+    alert("EHLİYET SINAVINA GİREBİLİRSİNİZ.");
+}
+else
+{
+    alert("EHLİYET SINAVINA GİREMEZSİNİZ.");
+}
