d1f7f9b53d35623accd65f7c2f186725ec5fa499
ÜMMÜGÜLSÜM SÖĞÜT <ummugulsum.sogut@ogr.gelisim.edu.tr>
2025-10-29 16:58:43 +0300

    Add files via upload

---
+++ veri_ogrenme3.js
@@ -0,0 +1,15 @@
+let adi="İrem";
+let soyad="Deneme";
+let sifre=123456;
+let durumu=true;
+let email="iyasar@ogr.gelisim.edu.tr";
+let ekbilgi=null;
+document.writeln(
+   " <h3><u> Kayıt Bilgisi Öğrenme</u></h3>","<br>",
+   "Adı: ", `${adi}`,"-",typeof(adi),"<br>",
+   "Soyadı: ",`${soyadı}`,"-",typeof(soyad),"<br>",
+   "Şifre :",`${sifre}`,"-",typeof(sifre),"<br>",
+   "Durumu: ",`${durumu}`,"-",typeof(durumu),"<br>",
+   "Email: "`${email}`,"-",typeof(email),"<br>",
+   "Ek Bilgi:",`${ekbilgi}`,"-",typeof(ekbilgi),"<br>"
+);
