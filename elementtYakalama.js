4c0be2b6155c0830e73e9bc90a6879efa838f3fb
ÜMMÜGÜLSÜM SÖĞÜT <ummugulsum.sogut@ogr.gelisim.edu.tr>
2025-11-14 12:29:15 +0300

    Add files via upload

---
+++ elementtYakalama.js
@@ -0,0 +1,12 @@
+let baslık=document.getElementById("ana-baslık");
+console.log("1.görev: "+baslık+"<br>");
+baslık.style.color="red";
+let ilkParagraf=document.querySelector(".bilgi-metni");
+console.log("2.görev: "+ilkParagraf+"<br>");
+ilkParagraf.style.backgroundColor="yellow";
+
+let link=document.querySelector("a");
+console.log("3.görev: "+link+"<br>");
+link.style.padding="60px";
+ let listeMaddeleri=document.querySelectorAll(".liste-maddeleri");
+ console.log("4.görev: "+listeMaddeleri+"<br>");
